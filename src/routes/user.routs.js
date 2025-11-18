e4d4baa6fa3c01e6b60b5c1cf96e92ff8cc39582
devbetao <humberto.software@gmail.com>
2025-11-18 08:12:56 -0300

    fix: Corrige erro de digitação na variável router.

--- src/routes/user.routs.js
+++ src/routes/user.routs.js
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import { Router } from "express";fix: Corrige sintaxe de import em user.routs.js e adiciona rota POST
 import userControllers from "../controller/user.controllers";
+
 const router = Router();
 
+router.post('/users', userControllers.createUserController
+    
+);
 
