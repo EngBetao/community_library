0dea89ac3eaa741678674e67102669ac38da3663
devbetao <humberto.software@gmail.com>
2025-11-22 14:41:19 -0300

    fix: Corrige erro na criação da tabela user e adiciona logs

--- src/repositories/user.repositries.js
+++ src/repositories/user.repositries.js
@@ -1,5 +1,6 @@
 import db from "../config/database.js";
 
+// Criação da tabela de Usuários
 db.run(`
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -8,20 +9,27 @@ db.run(`
     password TEXT NOT NULL,
     avatar TEXT
   )
-`);
+`, (err) => {
+  if (err) {
+    console.error("Erro na criação da tabela users:", err.message);
+  } else {
+    console.log("Tabela 'users' verificada/criada com sucesso.");
+  }
+});
 
 function createUserRepository(newUser) {
   return new Promise((resolve, reject) => {
     const { username, email, password, avatar } = newUser;
+    
     db.run(
-      `INSERT INTO users (username, email, password, avatar)
-       VALUES (?, ?, ?, ?)`,
+      `INSERT INTO users (username, email, password, avatar) VALUES (?, ?, ?, ?)`,
       [username, email, password, avatar],
       function (err) {
         if (err) {
           return reject(err);
         }
-        resolve({ id: this.lastID });
+        // O 'this.lastID' recupera o ID do usuário que acabou de ser criado
+        resolve({ id: this.lastID, ...newUser });
       }
     );
   });
